a51dbc3bd463f8e692e5cf501e9954c70cf12612
Aleksandr Skoriy <askoriy@users.noreply.github.com>
2020-10-08 11:00:17 +0300

    Add provider sources for compatibility with Terraform 0.13 (#3)

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    mysql = {
+      source = "terraform-providers/mysql"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
 provider mysql {
   endpoint       = "${var.endpoint}:${var.port}"
   username       = var.username
